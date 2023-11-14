833277ce7398be82436322db991651aca44f05a6
shetty-nithin <shettynithin744@gmail.com>
2023-11-14 20:42:34 +0530

    updated

--- apps/client-next/deploy.sh
+++ apps/client-next/deploy.sh
@@ -4,5 +4,5 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 cd home/ubuntu/end2endmonorepodemoproject
 git pull origin master
 npm build/yarn build
-pm2 stop nextapp
-pm2 start npm --name "nextapp" --run "start:nextapp"
+pm2 stop next
+pm2 start npm --name "next" --run "start:next"
