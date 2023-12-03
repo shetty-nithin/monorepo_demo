0779a5b6d7f11400eba28277229852cc901c90d2
shetty-nithin <shettynithin744@gmail.com>
2023-12-03 10:59:44 +0530

    testing

--- apps/express-backend/deploy.sh
+++ apps/express-backend/deploy.sh
@@ -2,8 +2,7 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.3.0/bin
 
 cd /home/ubuntu/monorepo_demo
-sudo git pull origin main
+git pull origin main
 npm run build
 pm2 stop express
-pm2 start npm --name "express" -- start:express
-echo "the end: $(date)"
+pm2 start npm --name "express" -- "start:express"
