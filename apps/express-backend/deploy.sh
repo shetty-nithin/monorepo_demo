39fa88d074642ceed5b2b08f6408f36ac9db6c72
shetty-nithin <shettynithin744@gmail.com>
2023-12-04 22:12:04 +0530

    testing

--- apps/express-backend/deploy.sh
+++ apps/express-backend/deploy.sh
@@ -5,5 +5,6 @@ cd /home/ubuntu/monorepo_demo
 git pull origin main
 npm install
 npm run build
+pm2 delete 0 1 2 3 4 5 6 7 8 9 10 11 12 13
 pm2 delete express
 pm2 start npm --name "express" -- "start:express"
