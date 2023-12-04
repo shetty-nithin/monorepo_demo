b9aa9d8ada2ac319bda9e78f554c3f21e2e32246
shetty-nithin <shettynithin744@gmail.com>
2023-12-04 23:56:55 +0530

    testing

--- apps/express-backend/src/index.ts
+++ apps/express-backend/src/index.ts
@@ -8,18 +8,18 @@ app.use(express.json());
 app.get("/", (req: any, res: any) => {
     let parsedUser = UserInput.safeParse(req.body);
     if(!parsedUser.success){
-        return res.send("simple route: working for sure2");
+        return res.send("simple route: working for sure3");
     }
-    return res.send("Nithin Shetty: its working alonee2");
+    return res.send("Nithin Shetty: its working alonee3");
 });
 
 
 app.get("/nithinshetty", (req: any, res: any) => {
     let parsedUser = UserInput.safeParse(req.body);
     if(!parsedUser.success){
-        return res.send("Nithin Shetty route: workign alone2");
+        return res.send("Nithin Shetty route: workign alone3");
     }
-    return res.send("Nithin Shetty: if failed: 2 routes: working alone2");
+    return res.send("Nithin Shetty: if failed: 2 routes: working alone3");
 });
 
 app.listen(PORT, () => {
