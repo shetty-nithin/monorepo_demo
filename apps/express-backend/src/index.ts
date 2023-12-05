2fd35a4df106312221428c105c8810a3f106a7fb
shetty-nithin <shettynithin744@gmail.com>
2023-12-05 22:14:55 +0530

    testing the CI/CD of express

--- apps/express-backend/src/index.ts
+++ apps/express-backend/src/index.ts
@@ -8,18 +8,18 @@ app.use(express.json());
 app.get("/", (req: any, res: any) => {
     let parsedUser = UserInput.safeParse(req.body);
     if(!parsedUser.success){
-        return res.send("Testing CI/CD3");
+        return res.send("Testing CI/CD4");
     }
-    return res.send("Testing CI/CD3");
+    return res.send("Testing CI/CD4");
 });
 
 
 app.get("/nithinshetty", (req: any, res: any) => {
     let parsedUser = UserInput.safeParse(req.body);
     if(!parsedUser.success){
-        return res.send("Testing CI/CD with Nithin Shetty3");
+        return res.send("Testing CI/CD with Nithin Shetty4");
     }
-    return res.send("Testing CI/CD with nsi3");
+    return res.send("Testing CI/CD with ns4");
 });
 
 app.listen(PORT, () => {
